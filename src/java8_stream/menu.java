2f24d7db5ff20430368130ed5f61f13dc482fd44
xianpeng.xia <951256916>
2019-03-19 08:46:59 +0800

    菜单筛选

---
+++ src/java8_stream/menu.java
@@ -0,0 +1,33 @@
+package java8_stream;
+
+import java8_stream.bean.Dish;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class menu {
+    public static void main(String[] args) {
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH)
+        );
+
+        //
+        List<String> threeHighCaloricDishNames = menu.stream()
+                .filter(d -> d.getCalories() > 450)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(toList());
+
+        System.out.println(threeHighCaloricDishNames);
+    }
+}
