2a21fb5f515d1cd4ba63d8fa5c2823eaa8a56c64
xianpeng.xia <951256916>
2019-03-21 00:59:14 +0800

    format

--- src/java8_stream/StreamDemo.java
+++ src/java8_stream/StreamDemo.java
@@ -11,8 +11,7 @@ import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
-public class StreamDemo
-{
+public class StreamDemo {
     public static void main(String[] args) {
         List<Dish> menu = Arrays.asList(
                 new Dish("pork", false, 800, Dish.Type.MEAT),
@@ -41,9 +40,9 @@ public class StreamDemo
         //s.forEach(System.out::println);
 
         List<Dish> dishes = menu.stream()
-        .filter(dish -> dish.getCalories()>300)
-        .skip(2)
-        .collect(toList());
+                .filter(dish -> dish.getCalories() > 300)
+                .skip(2)
+                .collect(toList());
 
         System.out.println(dishes);
 
@@ -54,50 +53,49 @@ public class StreamDemo
         System.out.println(dishNameLengths);
 
 
-        String[] words = {"hello","world"};
-        Stream<String> wordsStream  = Arrays.stream(words);
+        String[] words = {"hello", "world"};
+        Stream<String> wordsStream = Arrays.stream(words);
 
         List<String> uniqueCharacters = title
                 .stream()
-                .map(w->w.split(""))
+                .map(w -> w.split(""))
                 .flatMap(Arrays::stream)
                 .distinct()
                 .collect(toList());
         System.out.println(uniqueCharacters);
 
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         List<Integer> sqares = numbers
                 .stream()
-                .map(n->n*n)
+                .map(n -> n * n)
                 .collect(toList());
         System.out.println(sqares);
 
 
-        List<Integer> numbers1 = Arrays.asList(1,2,3);
-        List<Integer> numbers2 =  Arrays.asList(4,5);
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(4, 5);
 
         List<int[]> pairs = numbers1
                 .stream()
-                .flatMap(i-> numbers2
+                .flatMap(i -> numbers2
                         .stream()
-                        .filter(j->(i+j)%3==0)
-                        .map(j->new int[]{i,j}))
+                        .filter(j -> (i + j) % 3 == 0)
+                        .map(j -> new int[]{i, j}))
                 .collect(toList());
 
         for (int[] pair : pairs) {
-            System.out.println("("+pair[0]+","+pair[1]+")");
+            System.out.println("(" + pair[0] + "," + pair[1] + ")");
         }
 
 
-
-        if(menu.stream().anyMatch(Dish::isVegetarian)){
+        if (menu.stream().anyMatch(Dish::isVegetarian)) {
             System.out.println("has vegetarian");
         }
 
-        boolean isHealthy = menu.stream().allMatch(dish -> dish.getCalories()<1000);
+        boolean isHealthy = menu.stream().allMatch(dish -> dish.getCalories() < 1000);
         System.out.println(isHealthy);
 
-        isHealthy = menu.stream().noneMatch(dish -> dish.getCalories()>=1000);
+        isHealthy = menu.stream().noneMatch(dish -> dish.getCalories() >= 1000);
         System.out.println(isHealthy);
 
         Optional<Dish> dish = menu
@@ -112,12 +110,14 @@ public class StreamDemo
                 .ifPresent(d -> System.out.println(d.getName()));
 
 
-        List<Integer> someNumbers = Arrays.asList(1,2,3,4,5);
+        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
         Optional<Integer> fitstNumber = someNumbers
                 .stream()
-                .map(x->x*x)
-                .filter(x->x%3==0)
+                .map(x -> x * x)
+                .filter(x -> x % 3 == 0)
                 .findFirst();
         System.out.println(fitstNumber);
+
+
     }
 }
