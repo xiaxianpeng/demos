4018b1058bbf73989a795a20d375dd0ad6ccfd8c
xianpeng.xia <951256916>
2019-03-22 22:57:35 +0800

    filter sorted collect

--- src/java8_stream/AppleFilter.java
+++
@@ -1,8 +0,0 @@
-package java8_stream;
-
-public class AppleFilter {
-    public static void main(String[] args) {
-        System.out.printf(" ");
-    }
-
-}
---
+++ src/java8_stream/TraderTest.java
@@ -0,0 +1,36 @@
+package java8_stream;
+
+import java8_stream.bean.Trader;
+import java8_stream.bean.Transaction;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TraderTest {
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        List<Transaction> tr2011 = transactions.stream()
+                .filter(transaction -> transaction.getYear()==2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+        System.out.println(tr2011);
+
+
+
+    }
+}
