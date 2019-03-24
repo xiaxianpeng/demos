80cb843f5be656a7c5b99e4edb3dac085e58c34c
xianpeng.xia <951256916>
2019-03-24 19:00:44 +0800

    Stream.iterate() 无限流

--- src/java8_stream/StreamDemo.java
+++ src/java8_stream/StreamDemo.java
@@ -188,5 +188,9 @@ public class StreamDemo {
             e.printStackTrace();
         }
         System.out.println("uniqueWord size : " + uniqueWord);
+
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
     }
 }
