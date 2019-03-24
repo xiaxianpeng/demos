61770de88bf0e2fd648e79e9957461fa391056a1
xianpeng.xia <951256916>
2019-03-24 19:26:43 +0800

    Stream.iterate()生成斐波那伽数列

--- src/java8_stream/StreamDemo.java
+++ src/java8_stream/StreamDemo.java
@@ -192,5 +192,14 @@ public class StreamDemo {
         Stream.iterate(0, n -> n + 2)
                 .limit(10)
                 .forEach(System.out::println);
+
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(10)
+                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
+
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(20)
+                .map(t -> t[0])
+                .forEach(System.out::println);
     }
 }
