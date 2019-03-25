77e3a0885afe596c4d42361cb9db98e4b415045e
xianpeng.xia <951256916>
2019-03-25 23:38:21 +0800

    code style

--- src/java8_stream/StreamDemo.java
+++ src/java8_stream/StreamDemo.java
@@ -15,221 +15,213 @@ import static java.util.stream.Collectors.*;
 import static java.util.stream.Collectors.toList;
 
 public class StreamDemo {
-    public static void main(String[] args) {
-        List<Dish> menu = Arrays.asList(
-                new Dish("pork", false, 800, Dish.Type.MEAT),
-                new Dish("beef", false, 700, Dish.Type.MEAT),
-                new Dish("chicken", false, 400, Dish.Type.MEAT),
-                new Dish("french fries", true, 530, Dish.Type.OTHER),
-                new Dish("rice", true, 350, Dish.Type.OTHER),
-                new Dish("season fruit", true, 120, Dish.Type.OTHER),
-                new Dish("pizza", true, 550, Dish.Type.OTHER),
-                new Dish("prawns", false, 300, Dish.Type.FISH),
-                new Dish("salmon", false, 450, Dish.Type.FISH)
-        );
 
-        //
-        List<String> threeHighCaloricDishNames = menu.stream()
-                .filter(d -> d.getCalories() > 450)
-                .map(Dish::getName)
-                .limit(3)
-                .collect(toList());
+  public static void main(String[] args) {
+    List<Dish> menu = Arrays.asList(
+        new Dish("pork", false, 800, Dish.Type.MEAT),
+        new Dish("beef", false, 700, Dish.Type.MEAT),
+        new Dish("chicken", false, 400, Dish.Type.MEAT),
+        new Dish("french fries", true, 530, Dish.Type.OTHER),
+        new Dish("rice", true, 350, Dish.Type.OTHER),
+        new Dish("season fruit", true, 120, Dish.Type.OTHER),
+        new Dish("pizza", true, 550, Dish.Type.OTHER),
+        new Dish("prawns", false, 300, Dish.Type.FISH),
+        new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
 
-        System.out.println(threeHighCaloricDishNames);
+    //
+    List<String> threeHighCaloricDishNames = menu.stream()
+        .filter(d -> d.getCalories() > 450)
+        .map(Dish::getName)
+        .limit(3)
+        .collect(toList());
 
-        List<String> title = Arrays.asList("1", "s", "asd");
-        Stream<String> s = title.stream();
-        s.forEach(System.out::println);
-        //s.forEach(System.out::println);
+    System.out.println(threeHighCaloricDishNames);
 
-        List<Dish> dishes = menu.stream()
-                .filter(dish -> dish.getCalories() > 300)
-                .skip(2)
-                .collect(toList());
+    List<String> title = Arrays.asList("1", "s", "asd");
+    Stream<String> s = title.stream();
+    s.forEach(System.out::println);
+    //s.forEach(System.out::println);
 
-        System.out.println(dishes);
+    List<Dish> dishes = menu.stream()
+        .filter(dish -> dish.getCalories() > 300)
+        .skip(2)
+        .collect(toList());
 
-        List<Integer> dishNameLengths = menu.stream()
-                .map(Dish::getName)
-                .map(String::length)
-                .collect(toList());
-        System.out.println(dishNameLengths);
+    System.out.println(dishes);
 
+    List<Integer> dishNameLengths = menu.stream()
+        .map(Dish::getName)
+        .map(String::length)
+        .collect(toList());
+    System.out.println(dishNameLengths);
 
-        String[] words = {"hello", "world"};
-        Stream<String> wordsStream = Arrays.stream(words);
+    String[] words = {"hello", "world"};
+    Stream<String> wordsStream = Arrays.stream(words);
 
-        List<String> uniqueCharacters = title
-                .stream()
-                .map(w -> w.split(""))
-                .flatMap(Arrays::stream)
-                .distinct()
-                .collect(toList());
-        System.out.println(uniqueCharacters);
+    List<String> uniqueCharacters = title
+        .stream()
+        .map(w -> w.split(""))
+        .flatMap(Arrays::stream)
+        .distinct()
+        .collect(toList());
+    System.out.println(uniqueCharacters);
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        List<Integer> sqares = numbers
-                .stream()
-                .map(n -> n * n)
-                .collect(toList());
-        System.out.println(sqares);
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+    List<Integer> sqares = numbers
+        .stream()
+        .map(n -> n * n)
+        .collect(toList());
+    System.out.println(sqares);
 
+    List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+    List<Integer> numbers2 = Arrays.asList(4, 5);
 
-        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
-        List<Integer> numbers2 = Arrays.asList(4, 5);
+    List<int[]> pairs = numbers1
+        .stream()
+        .flatMap(i -> numbers2
+            .stream()
+            .filter(j -> (i + j) % 3 == 0)
+            .map(j -> new int[]{i, j}))
+        .collect(toList());
 
-        List<int[]> pairs = numbers1
-                .stream()
-                .flatMap(i -> numbers2
-                        .stream()
-                        .filter(j -> (i + j) % 3 == 0)
-                        .map(j -> new int[]{i, j}))
-                .collect(toList());
+    for (int[] pair : pairs) {
+      System.out.println("(" + pair[0] + "," + pair[1] + ")");
+    }
 
-        for (int[] pair : pairs) {
-            System.out.println("(" + pair[0] + "," + pair[1] + ")");
-        }
+    if (menu.stream().anyMatch(Dish::isVegetarian)) {
+      System.out.println("has vegetarian");
+    }
 
+    boolean isHealthy = menu.stream().allMatch(dish -> dish.getCalories() < 1000);
+    System.out.println(isHealthy);
 
-        if (menu.stream().anyMatch(Dish::isVegetarian)) {
-            System.out.println("has vegetarian");
-        }
+    isHealthy = menu.stream().noneMatch(dish -> dish.getCalories() >= 1000);
+    System.out.println(isHealthy);
 
-        boolean isHealthy = menu.stream().allMatch(dish -> dish.getCalories() < 1000);
-        System.out.println(isHealthy);
+    Optional<Dish> dish = menu
+        .stream()
+        .filter(Dish::isVegetarian)
+        .findAny();
+    System.out.println(dish);
 
-        isHealthy = menu.stream().noneMatch(dish -> dish.getCalories() >= 1000);
-        System.out.println(isHealthy);
+    menu.stream()
+        .filter(Dish::isVegetarian)
+        .findAny()
+        .ifPresent(d -> System.out.println(d.getName()));
 
-        Optional<Dish> dish = menu
-                .stream()
-                .filter(Dish::isVegetarian)
-                .findAny();
-        System.out.println(dish);
+    List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+    Optional<Integer> fitstNumber = someNumbers
+        .stream()
+        .map(x -> x * x)
+        .filter(x -> x % 3 == 0)
+        .findFirst();
+    System.out.println(fitstNumber);
 
-        menu.stream()
-                .filter(Dish::isVegetarian)
-                .findAny()
-                .ifPresent(d -> System.out.println(d.getName()));
+    int sum = someNumbers
+        .stream()
+        .reduce(0, (a, b) -> a + b);
 
+    int sum1 = someNumbers.stream().reduce(0, Integer::sum);
+    Optional<Integer> sum2 = someNumbers.stream().reduce(Integer::sum);
+    System.out.println(sum2);
 
-        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
-        Optional<Integer> fitstNumber = someNumbers
-                .stream()
-                .map(x -> x * x)
-                .filter(x -> x % 3 == 0)
-                .findFirst();
-        System.out.println(fitstNumber);
+    Optional<Integer> max = someNumbers.stream().reduce(Integer::max);
+    System.out.println("max: " + max);
 
-        int sum = someNumbers
-                .stream()
-                .reduce(0, (a, b) -> a + b);
+    Long count = menu.stream().count();
+    System.out.println("count" + count);
 
-        int sum1 = someNumbers.stream().reduce(0, Integer::sum);
-        Optional<Integer> sum2 = someNumbers.stream().reduce(Integer::sum);
-        System.out.println(sum2);
+    int calories = menu.stream()
+        .mapToInt(Dish::getCalories)
+        .sum();
+    System.out.println("calories = " + calories);
 
+    IntStream evenNumbers = IntStream.rangeClosed(1, 100)
+        .filter(n -> n % 2 == 0);
+    System.out.println(evenNumbers.count());
 
-        Optional<Integer> max = someNumbers.stream().reduce(Integer::max);
-        System.out.println("max: " + max);
-
-        Long count = menu.stream().count();
-        System.out.println("count" + count);
-
-
-
-        int calories = menu.stream()
-                .mapToInt(Dish::getCalories)
-                .sum();
-        System.out.println("calories = " + calories);
-
-        IntStream evenNumbers = IntStream.rangeClosed(1, 100)
-                .filter(n -> n % 2 == 0);
-        System.out.println(evenNumbers.count());
-
-        IntStream evenNumbers2 = IntStream.range(1, 100)
-                .filter(n -> n % 2 == 0);
-        System.out.println(evenNumbers2.count());
-
+    IntStream evenNumbers2 = IntStream.range(1, 100)
+        .filter(n -> n % 2 == 0);
+    System.out.println(evenNumbers2.count());
 
 //        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
 //                .flatMap(a ->
 //                        IntStream.rangeClosed(a, 100)
 //                                .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
 //                                .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
-        Stream<double[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
-                .flatMap(a -> IntStream.rangeClosed(a, 100)
-                        .mapToObj(b -> new double[]{a, b, Math.sqrt(a * a + b * b)}))
-                .filter(t -> Math.sqrt(t[0] * t[0] + t[1] * t[1]) % 1 == 0)
-                .limit(5);
+    Stream<double[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
+        .flatMap(a -> IntStream.rangeClosed(a, 100)
+            .mapToObj(b -> new double[]{a, b, Math.sqrt(a * a + b * b)}))
+        .filter(t -> Math.sqrt(t[0] * t[0] + t[1] * t[1]) % 1 == 0)
+        .limit(5);
 
+    pythagoreanTriples
+        .forEach(t -> System.out.println(t[0] + "," + t[1] + "," + t[2]));
 
-        pythagoreanTriples
-                .forEach(t -> System.out.println(t[0] + "," + t[1] + "," + t[2]));
+    Stream<String> stream = Stream.of("a", "b", "c", "d");
+    stream.map(String::toUpperCase).forEach(System.out::println);
 
+    Stream<String> emptyStream = Stream.empty();
 
-        Stream<String> stream = Stream.of("a", "b", "c", "d");
-        stream.map(String::toUpperCase).forEach(System.out::println);
+    int[] number = {2, 34, 5, 32, 45, 96};
+    int numberSum = Arrays.stream(number).sum();
+    System.out.println(numberSum);
 
-        Stream<String> emptyStream = Stream.empty();
-
-        int[] number = {2, 34, 5, 32, 45, 96};
-        int numberSum = Arrays.stream(number).sum();
-        System.out.println(numberSum);
-
-        long uniqueWord = 0;
-        try (Stream<String> lines = Files.lines(Paths.get("src/java8_stream/TraderTest.java"), Charset.defaultCharset())) {
-            uniqueWord = lines.flatMap(line -> Arrays.stream(line.split("")))
-                    .distinct()
-                    .count();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("uniqueWord size : " + uniqueWord);
-
-        Stream.iterate(0, n -> n + 2)
-                .limit(10)
-                .forEach(System.out::println);
-
-        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
-                .limit(10)
-                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
-
-        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
-                .limit(20)
-                .map(t -> t[0])
-                .forEach(System.out::println);
-
-        Stream.generate(Math::random)
-                .limit(5)
-                .forEach(System.out::println);
-
-        long howManyDishes = menu.stream().collect(Collectors.counting());
-        System.out.println("howManyDishes " + howManyDishes);
-        long howManyDishes1 = menu.stream().count();
-        System.out.println("howManyDishes " + howManyDishes1);
-
-        Comparator<Dish> dishCaloriedComparator = Comparator.comparingInt(Dish::getCalories);
-        Optional<Dish> mostCalorieDish = menu.stream()
-                .collect(maxBy(dishCaloriedComparator));
-        System.out.println("mostCalorieDish " + mostCalorieDish.toString());
-
-        int totalCalories = menu.stream()
-                .collect(Collectors.summingInt(Dish::getCalories));
-        System.out.println("totalCalories: " + totalCalories);
-
-        Double avgCalories = menu.stream()
-                .collect(Collectors.averagingInt(Dish::getCalories));
-        System.out.println("avgCalories " + avgCalories);
-
-        IntSummaryStatistics summaryStatistics = menu.stream()
-                .collect(Collectors.summarizingInt(Dish::getCalories));
-        System.out.println(summaryStatistics);
-
-        String shortMenu = menu.stream().map(Dish::getName).collect(Collectors.joining(", "));
-        System.out.println(shortMenu);
-
-        int sum4 = someNumbers.parallelStream().reduce(0,Integer::sum);
-        System.out.println("sum2 " +sum4);
+    long uniqueWord = 0;
+    try (Stream<String> lines = Files
+        .lines(Paths.get("src/java8_stream/TraderTest.java"), Charset.defaultCharset())) {
+      uniqueWord = lines.flatMap(line -> Arrays.stream(line.split("")))
+          .distinct()
+          .count();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    System.out.println("uniqueWord size : " + uniqueWord);
+
+    Stream.iterate(0, n -> n + 2)
+        .limit(10)
+        .forEach(System.out::println);
+
+    Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+        .limit(10)
+        .forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
+
+    Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+        .limit(20)
+        .map(t -> t[0])
+        .forEach(System.out::println);
+
+    Stream.generate(Math::random)
+        .limit(5)
+        .forEach(System.out::println);
+
+    long howManyDishes = menu.stream().collect(Collectors.counting());
+    System.out.println("howManyDishes " + howManyDishes);
+    long howManyDishes1 = menu.stream().count();
+    System.out.println("howManyDishes " + howManyDishes1);
+
+    Comparator<Dish> dishCaloriedComparator = Comparator.comparingInt(Dish::getCalories);
+    Optional<Dish> mostCalorieDish = menu.stream()
+        .collect(maxBy(dishCaloriedComparator));
+    System.out.println("mostCalorieDish " + mostCalorieDish.toString());
+
+    int totalCalories = menu.stream()
+        .collect(Collectors.summingInt(Dish::getCalories));
+    System.out.println("totalCalories: " + totalCalories);
+
+    Double avgCalories = menu.stream()
+        .collect(Collectors.averagingInt(Dish::getCalories));
+    System.out.println("avgCalories " + avgCalories);
+
+    IntSummaryStatistics summaryStatistics = menu.stream()
+        .collect(Collectors.summarizingInt(Dish::getCalories));
+    System.out.println(summaryStatistics);
+
+    String shortMenu = menu.stream().map(Dish::getName).collect(Collectors.joining(", "));
+    System.out.println(shortMenu);
+
+    int sum4 = someNumbers.parallelStream().reduce(0, Integer::sum);
+    System.out.println("sum2 " + sum4);
+  }
 }
--- src/java8_stream/TraderTest.java
+++ src/java8_stream/TraderTest.java
@@ -7,80 +7,80 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class TraderTest {
-    public static void main(String[] args) {
-        Trader raoul = new Trader("Raoul", "Cambridge");
-        Trader mario = new Trader("Mario", "Milan");
-        Trader alan = new Trader("Alan", "Cambridge");
-        Trader brian = new Trader("Brian", "Cambridge");
 
-        List<Transaction> transactions = Arrays.asList(
-                new Transaction(brian, 2011, 300),
-                new Transaction(raoul, 2012, 1000),
-                new Transaction(raoul, 2011, 400),
-                new Transaction(mario, 2012, 710),
-                new Transaction(mario, 2012, 700),
-                new Transaction(alan, 2012, 950)
+  public static void main(String[] args) {
+    Trader raoul = new Trader("Raoul", "Cambridge");
+    Trader mario = new Trader("Mario", "Milan");
+    Trader alan = new Trader("Alan", "Cambridge");
+    Trader brian = new Trader("Brian", "Cambridge");
+
+    List<Transaction> transactions = Arrays.asList(
+        new Transaction(brian, 2011, 300),
+        new Transaction(raoul, 2012, 1000),
+        new Transaction(raoul, 2011, 400),
+        new Transaction(mario, 2012, 710),
+        new Transaction(mario, 2012, 700),
+        new Transaction(alan, 2012, 950)
+    );
+
+    List<Transaction> tr2011 = transactions.stream()
+        .filter(transaction -> transaction.getYear() == 2011)
+        .sorted(Comparator.comparing(Transaction::getValue))
+        .collect(Collectors.toList());
+    System.out.println(tr2011);
+
+    List<String> citys = transactions.stream()
+        .map(transaction -> transaction.getTrader().getCity())
+        .distinct()
+        .collect(Collectors.toList());
+    System.out.println(citys.toString());
+
+    Set<String> city2 = transactions.stream()
+        .map(transaction -> transaction.getTrader().getCity())
+        .collect(Collectors.toSet());
+    System.out.println(city2.toString());
+
+    List<Trader> traders = transactions.stream()
+        .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
+        .map(Transaction::getTrader)
+        .distinct()
+        .sorted(Comparator.comparing(trader -> trader.getName()))
+        .collect(Collectors.toList());
+    System.out.println(traders.toString());
+
+    String names = transactions.stream()
+        .map(transaction -> transaction.getTrader().getName())
+        .distinct()
+        .sorted()
+        .reduce("", (a, b) -> a + b);
+    System.out.println(names);
+
+    String traderStr = transactions.stream()
+        .map(transaction -> transaction.getTrader().getName())
+        .distinct()
+        .sorted()
+        .collect(Collectors.joining());
+    System.out.println(traderStr);
+
+    boolean ifWorkOnMilan = transactions.stream()
+        .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
+    System.out.println(ifWorkOnMilan);
+
+    transactions.stream()
+        .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
+        .map(Transaction::getValue)
+        .forEach(System.out::println);
+    Optional<Integer> maxValue = transactions.stream()
+        .map(transaction -> transaction.getValue())
+        .reduce(Integer::max);
+    System.out.println("max : " + maxValue);
+
+    Optional<Transaction> minValue = transactions.stream()
+        .reduce((t1, t2) -> t1.getValue() < t2.getValue() ? t1 : t2
         );
+    Optional<Transaction> minVal = transactions.stream()
+        .min(Comparator.comparing(Transaction::getValue));
+    System.out.println(minVal);
 
-        List<Transaction> tr2011 = transactions.stream()
-                .filter(transaction -> transaction.getYear() == 2011)
-                .sorted(Comparator.comparing(Transaction::getValue))
-                .collect(Collectors.toList());
-        System.out.println(tr2011);
-
-        List<String> citys = transactions.stream()
-                .map(transaction -> transaction.getTrader().getCity())
-                .distinct()
-                .collect(Collectors.toList());
-        System.out.println(citys.toString());
-
-
-        Set<String> city2 = transactions.stream()
-                .map(transaction -> transaction.getTrader().getCity())
-                .collect(Collectors.toSet());
-        System.out.println(city2.toString());
-
-        List<Trader> traders = transactions.stream()
-                .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
-                .map(Transaction::getTrader)
-                .distinct()
-                .sorted(Comparator.comparing(trader -> trader.getName()))
-                .collect(Collectors.toList());
-        System.out.println(traders.toString());
-
-        String names = transactions.stream()
-                .map(transaction -> transaction.getTrader().getName())
-                .distinct()
-                .sorted()
-                .reduce("", (a, b) -> a + b);
-        System.out.println(names);
-
-        String traderStr = transactions.stream()
-                .map(transaction -> transaction.getTrader().getName())
-                .distinct()
-                .sorted()
-                .collect(Collectors.joining());
-        System.out.println(traderStr);
-
-        boolean ifWorkOnMilan = transactions.stream()
-                .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
-        System.out.println(ifWorkOnMilan);
-
-        transactions.stream()
-                .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
-                .map(Transaction::getValue)
-                .forEach(System.out::println);
-        Optional<Integer> maxValue = transactions.stream()
-                .map(transaction -> transaction.getValue())
-                .reduce(Integer::max);
-        System.out.println("max : " + maxValue);
-
-        Optional<Transaction> minValue = transactions.stream()
-                .reduce((t1, t2) -> t1.getValue() < t2.getValue() ? t1 : t2
-                );
-        Optional<Transaction> minVal = transactions.stream()
-                .min(Comparator.comparing(Transaction::getValue));
-        System.out.println(minVal);
-
-    }
+  }
 }
