ee97e582f16e4d49d74cb33d5343650cdaa6244a
xianpeng.xia <951256916>
2019-04-03 00:29:33 +0800

    使用并行流求和，与不实用并行流求和，迭代求和比较

--- src/main/java/java8_stream/ParallelStreamDemo.java
+++ src/main/java/java8_stream/ParallelStreamDemo.java
@@ -1,5 +1,6 @@
 package java8_stream;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 /**
@@ -11,6 +12,23 @@ public class ParallelStreamDemo {
     public static void main(String[] args) {
         long sum = parallelSum(200);
         System.out.println("sum = " + sum);
+        System.out.println(measureSumPerf(ParallelStreamDemo::iteraativeSum, 10000000));
+        System.out.println(measureSumPerf(ParallelStreamDemo::sequentialSum, 10000000));
+        System.out.println(measureSumPerf(ParallelStreamDemo::parallelSum, 10000000));
+    }
+
+    public static long sequentialSum(long n) {
+        return Stream.iterate(1L, i -> i + 1L)
+            .limit(n)
+            .reduce(0L, Long::sum);
+    }
+
+    public static long iteraativeSum(long n) {
+        long result = 0;
+        for (long i = 1L; i <= n; i++) {
+            result += i;
+        }
+        return result;
     }
 
     public static long parallelSum(long n) {
@@ -18,5 +36,18 @@ public class ParallelStreamDemo {
             .limit(n)
             .parallel()
             .reduce(0L, Long::sum);
+    }
+
+    public static long measureSumPerf(Function<Long, Long> addr, long n) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            long sum = addr.apply(n);
+            long duration = (System.nanoTime() - start) / 1000000;
+            if (duration < fastest) {
+                fastest = duration;
+            }
+        }
+        return fastest;
     }
 }
