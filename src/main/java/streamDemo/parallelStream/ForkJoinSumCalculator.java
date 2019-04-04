ca32cb6ab8e58148bfb04476208797c311527191
xianpeng.xia <951256916>
2019-04-04 08:58:45 +0800

    ForkJoinSumCalculator

---
+++ src/main/java/streamDemo/parallelStream/ForkJoinSumCalculator.java
@@ -0,0 +1,58 @@
+package streamDemo.parallelStream;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-04-03 08:46
+ */
+public class ForkJoinSumCalculator extends RecursiveTask<Long> {
+
+    private final long[] numbers;
+    private final int start;
+    private final int end;
+
+    public static final long THRESHOLD = 10000;
+
+    public ForkJoinSumCalculator(long[] numbers) {
+        this(numbers, 0, numbers.length);
+    }
+
+    private ForkJoinSumCalculator(long[] numbers, int start, int end) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        int length = end - start;
+        if (length <= THRESHOLD) {
+            return computeSequentially();
+        }
+        ForkJoinSumCalculator leftTask = new ForkJoinSumCalculator(numbers, start, start + length / 2);
+        leftTask.fork();
+        ForkJoinSumCalculator rightTask = new ForkJoinSumCalculator(numbers, start + length / 2, end);
+        rightTask.fork();
+        Long rightResult = rightTask.compute();
+        Long leftResult = leftTask.join();
+        return rightResult + leftResult;
+    }
+
+    private long computeSequentially() {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    public static long forkJoinSum(long n) {
+        long[] numbers = LongStream.rangeClosed(1, n).toArray();
+        ForkJoinTask<Long> task = new ForkJoinSumCalculator(numbers);
+        return new ForkJoinPool().invoke(task);
+    }
+}
