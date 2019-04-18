5714dea9aa32d88432a7d4ec39b97edabbce2061
xianpeng.xia <951256916>
2019-04-19 00:10:51 +0800

    环绕执行

--- src/main/java/refactor/Refactor.java
+++ src/main/java/refactor/Refactor.java
@@ -1,12 +1,16 @@
 package refactor;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by xianpeng.xia
  * on 2019-04-18 08:47
  */
 public class Refactor {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Runnable r1 = new Runnable() {
             @Override
             public void run() {
@@ -18,5 +22,21 @@ public class Refactor {
             System.out.println("hello");
         };
 
+        String oneLine = processFile((BufferedReader b) -> b.readLine());
+        String twoLine = processFile((BufferedReader b) -> b.readLine() + b.readLine());
+        System.out.println("one line : " + oneLine);
+        System.out.println("two line : " + twoLine);
+
+    }
+
+    public static String processFile(BufferedReaderProcessor p) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/roger/own/demos/demos.iml"))) {
+            return p.process(br);
+        }
+    }
+
+    public interface BufferedReaderProcessor {
+
+        String process(BufferedReader b) throws IOException;
     }
 }
