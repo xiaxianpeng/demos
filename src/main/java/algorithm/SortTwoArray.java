5b51bbc81286ff5ac93e4dfbc6a81e9cd172c604
夏先鹏 <951256916@qq.com>
2019-07-30 20:41:55 +0800

    合并有序数组题目

---
+++ src/main/java/algorithm/SortTwoArray.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+/**
+ * @author 夏先鹏
+ * @date 2019/07/30
+ * @time 20:34
+ *
+ *
+ * 给定两个有序整数数组num1和num2，将num2合并到num1中，使得num1为有序数组
+ */
+public class SortTwoArray {
+
+    private static int[] num1 = new int[]{1, 2, 3, 0, 0, 0};
+    private static int[] num2 = new int[]{2, 5, 6};
+    private static int m = 3;
+    private static int n = 3;
+    private static int[] out = new int[]{1, 2, 2, 3, 5, 6};
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
