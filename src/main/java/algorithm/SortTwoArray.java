2e380a7272fdae020e58c32f0c4331d45a4127e7
夏先鹏 <951256916@qq.com>
2019-08-02 11:15:06 +0800

    modify

--- src/main/java/algorithm/SortTwoArray.java
+++ src/main/java/algorithm/SortTwoArray.java
@@ -55,32 +55,13 @@ public class SortTwoArray {
         int p2 = n - 1;
         int p = m + n - 1;
         while ((p1 >= 0) && (p2 >= 0)) {
-            num1[p--] = (num1[p1] < num2[p2]) ? num2[p2--] : num1[p1 - 1];
+            num1[p--] = (num1[p1] < num2[p2]) ? num2[p2--] : num1[p1--];
         }
         System.arraycopy(num2, 0, num1, 0, p2 + 1);
         for (int i = 0; i < num1.length; i++) {
             System.out.println(num1[i]);
         }
         return num1;
-    }
-
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        // two get pointers for nums1 and nums2
-        int p1 = m - 1;
-        int p2 = n - 1;
-        // set pointer for nums1
-        int p = m + n - 1;
-
-        // while there are still elements to compare
-        while ((p1 >= 0) && (p2 >= 0))
-        // compare two elements from nums1 and nums2
-        // and add the largest one in nums1
-        {
-            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
-        }
-
-        // add missing elements from nums2
-        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
     }
 
 }
