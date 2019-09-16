b214d8538c35bc95d7fe83e08be2ab2944c7e4a5
夏先鹏 <951256916@qq.com>
2019-09-16 20:54:23 +0800

    当switch括号内的变量类型为String并且此变量为外部参数时，必须先进行null 判断。 否则npe

---
+++ src/main/java/sampleCode/Swich.java
@@ -0,0 +1,27 @@
+package sampleCode;
+
+/**
+ * @author 夏先鹏
+ * @date 2019/09/16
+ * @time 20:51
+ */
+public class Swich {
+
+    public static void main(String[] args) {
+        method(null);
+    }
+
+    public static void method(String param) {
+        switch (param) {
+            case "a":
+                System.out.println("it is a");
+                break;
+            case "null":
+                System.out.println("it is a");
+                break;
+            default:
+                System.out.println("default");
+                break;
+        }
+    }
+}
