3a74f0db428bf5bfe0287ce0d912ae7c65b06d70
夏先鹏 <951256916>
2020-04-19 20:25:15 +0800

    float test

---
+++ src/main/java/test/FloatTest.java
@@ -0,0 +1,38 @@
+package test;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/4/19 8:21 下午
+ */
+public class FloatTest {
+
+    public static void main(String[] args) {
+        float a = 1.0f - 0.9f;
+        float b = 0.9f - 0.8f;
+
+        if (a == b) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        Float m = Float.valueOf(a);
+        Float n = Float.valueOf(b);
+
+        if (m.equals(n)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        Double x = new Double(a);
+        Double y = new Double(b);
+
+        if (x.equals(y)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+    }
+}
