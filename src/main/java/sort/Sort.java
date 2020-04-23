a217c5fed07b080086dd6d8e660c88bf86ba2f66
夏先鹏 <951256916@qq.com>
2020-04-23 09:43:21 +0800

    选择排序

--- src/main/java/sort/Sort.java
+++ src/main/java/sort/Sort.java
@@ -41,11 +41,33 @@ public class Sort {
         }
     }
 
+
     void display(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + "\t");
         }
         System.out.println();
+    }
+
+    void selection(int[] array) {
+        // 1 寻找序列中的最小值
+        // 2 用当前位置的值交换最小值
+        // 3 对所有元素重复1 2
+
+        int temp, min;
+        for (int i = 0; i < array.length; i++) {
+            min = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -55,7 +77,8 @@ public class Sort {
         // 简单冒泡排序
         //// sort.bubbleSort(array);
         // 改进后的冒泡排序
-        sort.bubbleSortImproved(array);
+        //// sort.bubbleSortImproved(array);
+        sort.selection(array);
         sort.display(array);
     }
 }
