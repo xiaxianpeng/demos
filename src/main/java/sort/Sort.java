02c84145231e8c654badaeac4f2a6c75664e6c44
夏先鹏 <951256916>
2020-04-23 08:02:47 +0800

    改进后的冒泡排序(没有交换操作即排序完成)

--- src/main/java/sort/Sort.java
+++ src/main/java/sort/Sort.java
@@ -9,9 +9,9 @@ public class Sort {
     /**
      * 冒泡排序
      */
-    void BubbleSort(int[] array) {
+    void bubbleSort(int[] array) {
 
-        for (int pass = array.length ; pass >= 0; pass--) {
+        for (int pass = array.length; pass >= 0; pass--) {
 
             for (int i = 0; i < pass - 1; i++) {
                 if (array[i] > array[i + 1]) {
@@ -21,6 +21,23 @@ public class Sort {
                 }
             }
 
+        }
+    }
+
+    void bubbleSortImproved(int[] array) {
+        boolean swapped = true;
+
+        for (int pass = array.length; pass >= 0 && swapped; pass--) {
+            for (int i = 0; i < pass - 1; i++) {
+                swapped = false;
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+
+            }
         }
     }
 
@@ -35,7 +52,10 @@ public class Sort {
         int[] array = new int[]{5, 1, 3, 3, 55, 45, 33, 12, 76, 11, 56, 57, 15};
         Sort sort = new Sort();
         sort.display(array);
-        sort.BubbleSort(array);
+        // 简单冒泡排序
+        //// sort.bubbleSort(array);
+        // 改进后的冒泡排序
+        sort.bubbleSortImproved(array);
         sort.display(array);
     }
 }
